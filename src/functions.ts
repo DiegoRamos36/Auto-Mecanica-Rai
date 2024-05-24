59049454c6080379f819368db8d0b23200b29e47
Diego Ramos <diegoframos36@gmail.com>
2024-05-24 20:26:27 -0300

    pequenas mudancas

--- src/functions.ts
+++ src/functions.ts
@@ -1,7 +1,7 @@
 export function goService(msg: string) {
   if (msg) {
     const mensagem = `Olá, gostaria de fazer uma: ${msg}`;
-    const number = '+5521981734706';
+    const number = '+5521992581436';
     window.location.href = `https://wa.me/${number}/?text=${mensagem}`;
   } else {
     console.log('Sem serviço');
